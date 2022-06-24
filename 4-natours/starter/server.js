5250ccc69de4a690185b8284cddfb0462e0e848f
NonamedBread <lgh778923@gmail.com>
2022-06-24 17:43:49 +0900

    Updating Documents

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,11 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // console.log(process.env);
 
-const port = process.env.PORT || 4000;
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App runnig on port ${port}...`);
 });
