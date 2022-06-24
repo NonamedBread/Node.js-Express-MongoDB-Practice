04afa6fda81f53241fc0d1fb67b10e631008b56b
NonamedBread <lgh778923@gmail.com>
2022-06-24 17:56:05 +0900

    Deleting Documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -90,9 +90,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndRemove(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
