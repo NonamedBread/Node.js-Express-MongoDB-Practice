3eed6e0d32bfeecc596e549c690d3d3387866a93
NonamedBread <lgh778923@gmail.com>
2022-06-27 17:52:15 +0900

    fix:cors

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const path = require('path');
+
+const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+let corsOptions = {
+  origin: '*', // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+
+app.use(cors(corsOptions));
+
 // 1) MIDLEWARES
 // console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
+https: if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
